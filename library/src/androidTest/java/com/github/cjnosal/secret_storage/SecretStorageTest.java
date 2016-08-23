18f0611bb7103240335506407881406ceba29406
Conor Nosal <c.nosal@gmail.com>
2016-08-22 22:07:01 -0400

    Add SecretStorageTest

---
+++ library/src/androidTest/java/com/github/cjnosal/secret_storage/SecretStorageTest.java
@@ -0,0 +1,90 @@
+/*
+ *    Copyright 2016 Conor Nosal
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.github.cjnosal.secret_storage;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import com.github.cjnosal.secret_storage.keymanager.PasswordKeyManager;
+import com.github.cjnosal.secret_storage.keymanager.crypto.AndroidCrypto;
+import com.github.cjnosal.secret_storage.keymanager.crypto.Crypto;
+import com.github.cjnosal.secret_storage.keymanager.defaults.DefaultManagers;
+import com.github.cjnosal.secret_storage.keymanager.defaults.DefaultSpecs;
+import com.github.cjnosal.secret_storage.keymanager.defaults.DefaultStrategies;
+import com.github.cjnosal.secret_storage.storage.DataStorage;
+import com.github.cjnosal.secret_storage.storage.FileStorage;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import static junit.framework.Assert.assertEquals;
+
+public class SecretStorageTest {
+
+    Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+    Crypto crypto;
+    AndroidCrypto androidCrypto;
+    DataStorage configStorage;
+    DataStorage keyStorage;
+    DataStorage dataStorage;
+    DefaultManagers defaultManagers;
+
+    @Before
+    public void setup() throws IOException {
+        context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        crypto = new Crypto();
+        androidCrypto = new AndroidCrypto();
+        configStorage = new FileStorage(context.getFilesDir() + "/testConfig");
+        keyStorage = new FileStorage(context.getFilesDir() + "/testKeys");
+        dataStorage = new FileStorage(context.getFilesDir() + "/testData");
+        defaultManagers = new DefaultManagers();
+        configStorage.clear();
+    }
+
+    @Test
+    public void createWithPassword() throws IOException, GeneralSecurityException {
+        SecretStorage secretStorage = new SecretStorage(context, "id", configStorage, dataStorage, "password");
+        secretStorage.store("mysecret", "message".getBytes());
+        assertEquals(new String(secretStorage.load("mysecret")), "message");
+    }
+
+    @Test
+    public void createWithoutPassword() throws IOException, GeneralSecurityException {
+        SecretStorage secretStorage = new SecretStorage(context, "id", configStorage, dataStorage, null);
+        secretStorage.store("mysecret", "message".getBytes());
+        assertEquals(new String(secretStorage.load("mysecret")), "message");
+    }
+
+    @Test
+    public void createWithManager() throws IOException, GeneralSecurityException {
+        PasswordKeyManager manager = new PasswordKeyManager(crypto,
+                DefaultStrategies.getDataProtectionStrategy(crypto),
+                DefaultSpecs.getPbkdf2WithHmacShaDerivationSpec(),
+                DefaultStrategies.getPasswordBasedKeyProtectionStrategy(crypto),
+                keyStorage,
+                configStorage);
+        manager.unlock("password");
+
+        SecretStorage secretStorage = new SecretStorage(context, "id", configStorage, manager, dataStorage);
+        secretStorage.store("mysecret", "message".getBytes());
+        assertEquals(new String(secretStorage.load("mysecret")), "message");
+    }
+
+}
